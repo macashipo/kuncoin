7d208c144de0aaad7b3f1d1c59d0642780aaeec0
macashipo <macashipo@yahoo.com>
2022-01-07 12:51:57 +0700

    Update Token.sol

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -6,6 +6,6 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract KunCoin is ERC721 {
     constructor() ERC721("KunCoin", "KUN") {
-        _safeMint(msg.sender,10000000);
+        _safeMint(msg.sender,10000000*10**18);
     }
 }
