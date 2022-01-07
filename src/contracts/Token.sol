31988996bfe490a9c245229bfc99521712a895d2
macashipo <macashipo@yahoo.com>
2022-01-07 10:14:35 +0700

    Create Token.sol

---
+++ src/contracts/Token.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract KunCoin is ERC721 {
+    constructor() ERC721("KunCoin", "KUN") {
+    }
+}
